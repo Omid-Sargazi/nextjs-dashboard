f80074e923d28618a23aee6def7b6c6a403cea14
OmidSargazi <om.sargazi@gmail.com>
2024-03-24 01:14:27 +0330

    not found page and not found function

--- app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {notFound} from "next/navigation"
 
 const ReviewDetail = ({params}:{
     params:{
@@ -6,6 +7,10 @@ const ReviewDetail = ({params}:{
         reviewsId:string,
     }
 }) => {
+    if((parseInt(params.reviewsId)>1000)){
+        notFound()
+
+    }
   return (
     <div>
       Review {params.reviewsId} for prodcut {params.productId}
