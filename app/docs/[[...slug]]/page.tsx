73a4df6632744c53a780b91c82f40f93a09efd53
OmidSargazi <om.sargazi@gmail.com>
2024-03-23 23:26:13 +0330

    catch all segments

---
+++ app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Docs = ({params}:{
+    params:{
+        slug:string[]
+    }
+}) => {
+    if(params.slug?.length===2){
+        return <h1>
+            Viewing docs for features {params.slug[0]} and concept {params.slug[1]}
+        </h1>
+    }else if(params.slug?.length===1){
+        return <h1>Viewing docs for features {params.slug[0]} </h1>
+    }
+
+  return (
+    <div>
+      <h1>Docs home page</h1>
+    </div>
+  )
+}
+
+export default Docs
